41eba3cdbc67428fffe5de76f1647d9a5e88007a
Nacho Álvarez Ruiz <neonigma@gmail.com>
2014-08-05 23:20:08 +0200

    Remove whitespaces

--- java/com/qualcomm/vuforia/UpdateTarget.java
+++ java/com/qualcomm/vuforia/UpdateTarget.java
@@ -29,7 +29,7 @@ public class UpdateTarget {
 	//Server Keys
 	private String accessKey = "[ server access key ]";
 	private String secretKey = "[ server secret key ]";
-		
+
 	private String targetId = "[ target id ]";
 	private String url = "https://vws.vuforia.com";
 
@@ -38,27 +38,27 @@ public class UpdateTarget {
 		HttpClient client = new DefaultHttpClient();
 		putRequest.setURI(new URI(url + "/targets/" + targetId));
 		JSONObject requestBody = new JSONObject();
-		
+
 		setRequestBody(requestBody);
 		putRequest.setEntity(new StringEntity(requestBody.toString()));
 		setHeaders(putRequest); // Must be done after setting the body
-		
+
 		HttpResponse response = client.execute(putRequest);
 		System.out.println(EntityUtils.toString(response.getEntity()));
 	}
-	
+
 	private void setRequestBody(JSONObject requestBody) throws IOException, JSONException {
 		//requestBody.put("active_flag", true); // Optional
 		requestBody.put("application_metadata", Base64.encodeBase64String("Vuforia test metadata".getBytes())); // Optional
 	}
-	
+
 	private void setHeaders(HttpUriRequest request) {
 		SignatureBuilder sb = new SignatureBuilder();
 		request.setHeader(new BasicHeader("Date", DateUtils.formatDate(new Date()).replaceFirst("[+]00:00$", "")));
 		request.setHeader(new BasicHeader("Content-Type", "application/json"));
 		request.setHeader("Authorization", "VWS " + accessKey + ":" + sb.tmsSignature(request, secretKey));
 	}
-	
+
 	public static void main(String[] args) throws URISyntaxException, ClientProtocolException, IOException, JSONException {
 		UpdateTarget u = new UpdateTarget();
 		u.updateTarget();
